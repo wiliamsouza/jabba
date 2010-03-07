c37a025c1cd44e1c195ecd652e33f360c1a0d6eb
Wiliam Souza <wiliamsouza83@gmail.com>
2010-03-06 21:40:40 -0300

    Added admin site for helpdesk

---
+++ jabba/helpdesk/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from helpdesk.models import Team, Task, Context, Note, Attachment
+
+
+admin.site.register(Team)
+admin.site.register(Task)
+#admin.site.register(Project)
+admin.site.register(Context)
+admin.site.register(Note)
+admin.site.register(Attachment)
