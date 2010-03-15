7d5958a5a2f90fed7e7f72fa9faab24257adafae
Wiliam Souza <wiliamsouza83@gmail.com>
2010-03-15 16:14:18 -0300

    Removed project class

--- jabba/helpdesk/admin.py
+++ jabba/helpdesk/admin.py
@@ -5,7 +5,6 @@ from helpdesk.models import Team, Task, Context, Note, Attachment
 
 admin.site.register(Team)
 admin.site.register(Task)
-#admin.site.register(Project)
 admin.site.register(Context)
 admin.site.register(Note)
 admin.site.register(Attachment)
